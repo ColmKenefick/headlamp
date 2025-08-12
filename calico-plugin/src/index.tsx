0593ed8cae3fc70a90e5ea9d53bb42c060018c14
Antony Guinard <antony@tigera.io>
2025-08-13 00:45:50 +0100

    Fairly dirty tabs too

--- calico-plugin/src/index.tsx
+++ calico-plugin/src/index.tsx
@@ -15,18 +15,74 @@
  */
 import { registerRoute, registerSidebarEntry } from '@kinvolk/headlamp-plugin/lib';
 // @ts-ignore
+import {
+  Box,
+} from '@mui/material';
+
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import FlowsView from './components/FlowsView/index.js';
+import { useState } from 'react';
+import CalicoCloudToken from './components/CalicoCloudToken';
+import FlowsView from './components/FlowsView';
 import { CalicoIcon } from './utils/customIcons';
 
 const queryClient = new QueryClient();
 
-export default function CalicoRoot() {
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+// Taken from https://mui.com/material-ui/react-tabs/#BasicTabs.tsx
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <FlowsView />
-    </QueryClientProvider>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
   );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function CalicoRoot() {
+  const [tab, setTab] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
+  };
+
+  return <QueryClientProvider client={queryClient}>
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Tabs value={tab} onChange={handleChange} aria-label="basic tabs example">
+        <Tab label="Calico Flows" {...a11yProps(0)} />
+        <Tab label="Calico Policies Stats" {...a11yProps(1)} />
+      </Tabs>
+    </Box>
+    <CustomTabPanel value={tab} index={0}>
+      <FlowsView />
+    </CustomTabPanel>
+    <CustomTabPanel value={tab} index={1}>
+      <CalicoCloudToken />
+    </CustomTabPanel>
+  </QueryClientProvider>
 }
 
 registerRoute({
